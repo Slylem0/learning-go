d58c0395cb09815b56e031c63d15dadc3196f5bb
Pablo Nicolás Marín <pnicolasmarin2007@gmail.com>
2026-09-12 00:37:14 -0500

    idk

--- example/main.go
+++ example/main.go
@@ -10,11 +10,15 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-
-	message, err := greetings.Hello("")
+	names := []string{"alex", "memento", "gfustavo", "pablo"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// message, err := greetings.Hello("moon")
+	// if err != nil {
+	//	log.Fatal(err)
+	// }
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
